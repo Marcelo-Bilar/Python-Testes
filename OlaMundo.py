f40e0a00c689c5734255eb15379586581fe75e52
Marcelo Bilar Rodrigues <marcelo.bilar2@gmail.com>
2021-07-14 11:56:21 -0300

    pequenas alteraçoes

---
+++ OlaMundo.py
@@ -0,0 +1,20 @@
+def calcular(n1, n2, o):
+    if o == '+':
+        return n1 + n2
+    elif o == '-':
+        return n1 - n2
+    elif o == '*':
+        return n1 * n2
+    elif o == '/':
+        return n1 / n2
+    elif o == '%':
+        return n1 % n2
+    elif o == '**':
+        return n1 ** n2
+
+if __name__ == '__main__':
+    n1 = int(input('Digite o primeiro num: '))
+    o = input("digite a operação: ")
+    n2 = int(input('Digite o segundo num: '))
+    resultado = calcular(n1, n2, o)
+    print(f'o resultado da operação é {resultado}')
